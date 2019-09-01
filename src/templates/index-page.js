c6ff18c241b2ff39c7c5e148d45b6e7179fcff06
ali neer <ali@alineer.com>
2019-09-01 02:52:51 -0400

    styling with colors

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,9 +40,9 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+              '#992409 0.5rem 0px 0px, #992409 -0.5rem 0px 0px',
+            backgroundColor: '#992409',
+            color: '#fef6d0',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -53,9 +53,9 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+              '#992409 0.5rem 0px 0px, #992409 -0.5rem 0px 0px',
+            backgroundColor: '#992409',
+            color: '#fef6d0',
             lineHeight: '1',
             padding: '0.25em',
           }}

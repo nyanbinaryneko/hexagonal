5818e351f96f62e09c840bb8c1862a80bda0f7b1
ali neer <ali@alineer.com>
2019-09-04 22:02:09 -0400

    favicos and shit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,24 +18,24 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix("/")}img/apple-touch-icon.png`}
+          href={`${withPrefix("/")}img/apple-touch.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-32x32.png`}
+          href={`${withPrefix("/")}img/favicon-32x32.ico`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix("/")}img/favicon-16x16.png`}
+          href={`${withPrefix("/")}img/favicon-16x16.ico`}
           sizes="16x16"
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
+          href={`${withPrefix("/")}img/safari-pinned-tab.png`}
           color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />

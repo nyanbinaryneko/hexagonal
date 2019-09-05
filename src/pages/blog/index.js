c25f4dca6bd228b7cc1ad2421d38f0e89ce163f5
ali neer <ali@alineer.com>
2019-09-04 21:23:20 -0400

    set up for final styling touches

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,6 +3,11 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
+
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomByte = () => randomNumber(0, 255)
+const randomPercent = () => (randomNumber(50, 100) * 0.01).toFixed(2)
+const randomCssRgba = () => `rgba(${[randomByte(), randomByte(), randomByte(), randomPercent()].join(',')})`
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
@@ -10,20 +15,24 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/blog-index.svg'), linear-gradient(${randomCssRgba()}, ${randomCssRgba()})`,
           }}
         >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
+          <div className="box"
+          style={{
+            backgroundColor: '#fef6d0'
+          }}
           >
-            Latest Stories
-          </h1>
+            <h1
+              className="has-text-weight-bold is-size-1"
+              style={{
+                color: '#992409',
+                padding: '1rem',
+              }}
+            >
+              Latest Stories
+            </h1>
+          </div>
         </div>
         <section className="section">
           <div className="container">

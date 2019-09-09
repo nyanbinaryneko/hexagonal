b75e3760739d4008b7ecdf6c455fe171f377a1ae
ali neer <ali@alineer.com>
2019-09-09 19:55:40 -0400

    remove unused vars to make the compiler shut up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {

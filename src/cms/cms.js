0688a988e0775f079cf6f7dfce1903281faabaa6
ali neer <ali@alineer.com>
2019-09-09 22:05:52 -0400

    remove commented out import

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,6 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-//import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
